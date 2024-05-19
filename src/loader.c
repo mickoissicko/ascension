698629341dbab71821a68f842b85d9a8de4ef3e2
mickoissicko <defualtusermick@gmail.com>
2024-05-19 18:13:28 +0200

    loader.c function

---
+++ src/loader.c
@@ -0,0 +1,14 @@
+// TODO:
+// implement loader
+
+void Loader()
+{
+    #ifdef __APPLE__
+        // do something
+    #elif __WIN32__
+        // do something
+    #else
+        // do something
+    #endif
+}
+
