435550f05f222344f03f86b5d0ecd5ca7b682e90
mickoissicko <defualtusermick@gmail.com>
2024-09-09 02:36:02 +0500

    processor

--- src/proc.c
+++ src/proc.c
@@ -1,13 +1,30 @@
+#define NORMAL_EXIT 500
+#define STD_WRITE
+#define MAIN
+#define IO
+#include "shared/incl.h"
+#include "std/symbols.h"
 #include "std/str/sub.h"
-#include "decl/decl.h"
-#include "sh/shell.h"
+#include "std/str/len.h"
+#include "std/str/cmp.h"
+#include "std/io.h"
 
-int proc(char inpt[])
+int proc(char* s)
 {
-    char* _retval = run(inpt);
+    if (cmpstr(s, var(0)) == 0)
+        return 0;
 
-    if (cmpstr(_retval, NO_EXIT_EVENT) != 0)
-        return makeint((_retval + lenstr(var(0)) + 1));
+    if (findsub(s, var(4)) != NULL)
+        stdparse(s, LS);
+
+    if (findsub(s, var(3)) != NULL)
+        stdparse(s, CD);
+
+    if (findsub(s, var(5)) != NULL)
+        stdparse(s, OPN);
+
+    if (findsub(s, var(6)) != NULL)
+        stdparse(s, RUN);
 
     return NORMAL_EXIT;
 }
