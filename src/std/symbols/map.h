df98690d202b9e10d29a28cb6c4a6e19da8214ed
mickoissicko <defualtusermick@gmail.com>
2024-09-09 02:33:35 +0500

    update maps n symbols

--- src/std/symbols/map.h
+++ src/std/symbols/map.h
@@ -9,6 +9,10 @@ static inline char* var(int var)
     if (var == 0) RET EXIT;
     if (var == 1) RET ECHO;
     if (var == 2) RET DEF;
+    if (var == 3) RET CD;
+    if (var == 4) RET LS;
+    if (var == 5) RET OPN;
+    if (var == 6) RET RUN;
 }
 
 #endif
--- src/std/symbols/sym.h
+++ src/std/symbols/sym.h
@@ -2,8 +2,12 @@
 #define SYM_H
 
 #define PROMPT "-> "
+#define OPN "open "
 #define EXIT "exit"
 #define ECHO "echo"
 #define DEF "def"
+#define CD "cd "
+#define LS "ls"
+#define RUN "run "
 
 #endif
