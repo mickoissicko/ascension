f4872851e31cc99492b38d88bf0c6a7e58b46c7d
mickoissicko <defualtusermick@gmail.com>
2024-09-09 02:32:50 +0500

    add build files and remove 1

--- bin/make.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-cd ..
-chmod u+x build.sh
-./build.sh
--- build.sh
+++ build.sh
@@ -1,2 +1,16 @@
-#!/bin/bash
-gcc -o bin/bin src/main.c src/proc.c src/std/mem/alloc.
+cd src/
+
+gcc -o ../bin/bin               \
+    main.c                      \
+    proc.c                      \
+    compiler/cmds/ls.c          \
+    compiler/cmds/sysrun.c      \
+    compiler/cmds/cd.c          \
+    compiler/helper/chklsarg.c  \
+    compiler/helper/err.c       \
+    compiler/helper/getsz.c     \
+    compiler/getmod.c           \
+    compiler/prepare.c          \
+    compiler/read.c             \
+    compiler/parse.c            \
+    compiler/stdparse.c
