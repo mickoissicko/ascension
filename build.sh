060a21a184ca28d0cb6cee2f849162532a75d262
mickoissicko <defualtusermick@gmail.com>
2024-09-11 14:04:23 +0200

    update build

--- build.sh
+++ build.sh
@@ -1,16 +1,16 @@
-cd src/
+cd src/ || return 1
 
-gcc -o ../bin/bin               \
-    main.c                      \
-    proc.c                      \
-    compiler/cmds/ls.c          \
-    compiler/cmds/sysrun.c      \
-    compiler/cmds/cd.c          \
-    compiler/helper/chklsarg.c  \
-    compiler/helper/err.c       \
-    compiler/helper/getsz.c     \
-    compiler/getmod.c           \
-    compiler/prepare.c          \
-    compiler/read.c             \
-    compiler/parse.c            \
-    compiler/stdparse.c
+gcc -o ../bin/bin                  \
+    main.c                         \
+    proc.c                         \
+    interpreter/cmds/ls.c          \
+    interpreter/cmds/sysrun.c      \
+    interpreter/cmds/cd.c          \
+    interpreter/helper/chklsarg.c  \
+    interpreter/helper/err.c       \
+    interpreter/helper/getsz.c     \
+    interpreter/getmod.c           \
+    interpreter/prepare.c          \
+    interpreter/read.c             \
+    interpreter/interpreter.c      \
+    interpreter/stdparse.c
